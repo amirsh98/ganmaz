ae3496de5a48806a7bef26b3ed328276287b2b2b
Saeed Shahrivari <saeed.shahrivari@gmail.com>
2017-04-26 01:57:41 +0430

    Added command line params.

--- src/main/java/ir/toolki/ganmaz/Rest.kt
+++ src/main/java/ir/toolki/ganmaz/Rest.kt
@@ -3,16 +3,31 @@ package ir.toolki.ganmaz
 import ir.toolki.ganmaz.classification.DataSet
 import ir.toolki.ganmaz.classification.Document
 import ir.toolki.ganmaz.sample.News
+import org.kohsuke.args4j.CmdLineException
+import org.kohsuke.args4j.CmdLineParser
 import org.kohsuke.args4j.Option
 import spark.Response
 import spark.Spark.*
 
 
 fun main(args: Array<String>) {
-    val port = 1234
-    val k = 5
-    val dataset = DataSet(News.readSamples())
-    port(port)
+    val opts = CmdOpts()
+    val parser = CmdLineParser(opts)
+    try {
+        parser.parseArgument(*args)
+    } catch (e: CmdLineException) {
+        parser.printUsage(System.err)
+    }
+
+    port(opts.port)
+
+    var dataset: DataSet
+    if (opts.json.isEmpty()) {
+        println("No JSON given! Reading default json....")
+        dataset = DataSet(News.readSamples())
+    } else
+        dataset = DataSet(News.readFile(opts.json))
+
 
     fun doResponse(text: String, resp: Response): String {
         resp.status(200)
@@ -22,7 +37,7 @@ fun main(args: Array<String>) {
             override val label = ""
             override fun body() = text
         }
-        return dataset.classify(doc, k)
+        return dataset.classify(doc, opts.k)
     }
 
 
@@ -31,9 +46,11 @@ fun main(args: Array<String>) {
 
 }
 
-class cmdOpts(
+class CmdOpts(
         @Option(name = "port", usage = "The port to listen")
         val port: Int = 5050,
-        @Option(name = "port", usage = "The port to listen")
-        json: String = ""
+        @Option(name = "k", usage = "Number of Neighbors for classifier.")
+        val k: Int = 5,
+        @Option(name = "json", usage = "The path of json file")
+        val json: String = ""
 )
--- src/main/java/ir/toolki/ganmaz/sample/News.kt
+++ src/main/java/ir/toolki/ganmaz/sample/News.kt
@@ -1,9 +1,11 @@
 package ir.toolki.ganmaz.sample
 
-import ir.toolki.ganmaz.classification.Document
 import com.google.common.io.CharStreams
+import com.google.common.io.Files
 import com.google.gson.GsonBuilder
 import com.google.gson.annotations.SerializedName
+import ir.toolki.ganmaz.classification.Document
+import java.io.File
 
 class News(val title: String,
            override val id: Int,
@@ -13,13 +15,21 @@ class News(val title: String,
 
     override fun body() = "$title\n$title\n$body"
 
-    companion object Resources {
+    companion object {
+        val gson = GsonBuilder().setLenient().create()
         fun readSamples(): List<News> {
-            val gson = GsonBuilder().setLenient().create()
-            var lines = CharStreams.readLines(News::class.java.getResourceAsStream("/sample.json").bufferedReader())
-            // remove empty lines
-            lines = lines.filter { it.trim().isNotEmpty() }.toList()
-            return lines.map { gson.fromJson(it, News::class.java) }.toList()
+            return CharStreams.readLines(News::class.java.getResourceAsStream("/sample.json").bufferedReader())
+                    // remove empty lines
+                    .filter { it.trim().isNotEmpty() }
+                    .map { gson.fromJson(it, News::class.java) }.toList()
+        }
+
+
+        fun readFile(path: String): List<News> {
+            return Files.readLines(File(path), Charsets.UTF_8)
+                    // remove empty lines
+                    .filter { it.trim().isNotEmpty() }
+                    .map { gson.fromJson(it, News::class.java) }.toList()
         }
     }
 }
